395eceda64af02704510f338d3519347cc174b55
honchan <honkangchan@gmail.com>
2021-05-10 20:35:54 +0800

    add exercise instructions

---
+++ src/exercise.js
@@ -0,0 +1,14 @@
+/*
+  Add following behaviour do this without using RTK to get practice on immutable updates
+  https://redux.js.org/recipes/structuring-reducers/immutable-update-patterns
+  
+  Exend the actions / reducers in redux folder
+    - edit a message
+    - delete a message
+    - clear messages
+
+  on the UI side no need to have styling
+  for Edit , can add a button within the item
+  for Delete, can add a button within the item
+  for Clear, can add a button at top
+*/
